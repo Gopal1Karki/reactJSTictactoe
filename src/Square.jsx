5220c661efa2ea2255f297fde12d5451f09b31f4
Gopal1Karki <karkipawan305@gmail.com>
2023-05-10 11:38:58 +0545

    initialcommit

---
+++ src/Square.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Square = (props) => {
+    return (
+        <>
+
+            <div  onClick={props.onClick} className="square">
+
+                <h4>{props.value}</h4>
+            </div>
+        </>
+    )
+}
+
+export default Square;
